a42fcdd71772f02b0e20a0a42569ea70e50e98c3
Arjun k <arjunksukesh99@gmail.com>
2024-05-07 12:43:15 +0530

    conversations functionality

--- api/routes/conversation.route.js
+++ api/routes/conversation.route.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { conversationFn } from '../controller/conversation.controller.js';
+import { 
+    getConversations,
+    createConversation,
+    getSingleConversation,
+    updateConversation 
+} from '../controller/conversation.controller.js';
+import { verifyToken } from '../middleware/jwt.js';
 
 const router = express.Router();
 
-router.get('/conversation', conversationFn )
+router.post('/',verifyToken ,createConversation )
+router.get('/',verifyToken ,getConversations )
+router.get('/single/:id',verifyToken ,getSingleConversation )
+router.put('/:id',verifyToken ,updateConversation )
+
 
 export default router;
--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,44 +1,74 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
+import newRequest from '../utils/newRequest.js';
+import moment from 'moment';
 
 export default function Messages() {
 
-  const message = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-  maxime cum corporis esse aspernatur laborum dolorum? Animi
-  molestias aliquam, cum nesciunt, aut, ut quam vitae saepe repellat`
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['conversations'],
+    queryFn: () =>
+      newRequest.get('/conversation')
+        .then(res => res.data)
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn : (id) => {
+      return newRequest.put(`/conversation/${id}`);
+    },
+    onSuccess : () => {
+      queryClient.invalidateQueries(["conversations"])
+    }
+
+  });
+
+  const hanleRead = (id) => {
+    mutation.mutate(id);
+  }
+
+
+
   return (
     <div className='w-full flex justify-center '>
-      <div className='w-[1430px] flex flex-col gap-2 p-2'>
-        <div className='flex items-center justify-between mt-2'>
-          <h1 className='text-xl font-bold'>Messages</h1>
+      {isLoading
+        ? ('loading')
+        : (error)
+          ? ('error')
+          : (<div className='w-[1430px] flex flex-col gap-2 p-2'>
+            <div className='flex items-center justify-between mt-2'>
+              <h1 className='text-xl font-bold'>Messages</h1>
 
-        </div>
-        <table className=''>
-          <tr className='h-[50px] '>
-            <th className='text-left pl-4'>Buyer</th>
-            <th className='text-left pl-4'>Last Message</th>
-            <th className='text-left pl-4'>Date</th>
-            <th className='text-left pl-4'>Action</th>
-          </tr>
-          <tr className='h-[100px] bg-green-100'>
-            <td className='pl-6'>John Doe</td>
-            <td className='pl-4'><Link to={'/message/123'}>{message.substring(0, 100)}...</Link></td>
-            <td className='pl-6'>1 hour ago</td>
-            <td className='pl-5'>
-              <button className='bg-green-500 text-white font-semibold p-1 rounded'>Mark as Read</button>
-            </td>
+            </div>
+            <table className='border-separate' >
+              <tbody>
 
-          </tr>
-          <tr className='h-[100px] bg-green-100'>
-            <td className='pl-6'>John Doe </td>
-            <td className='pl-4 '><Link to={'/message/123'}>{message.substring(0, 100)}...</Link></td>
-            <td className='pl-6'>1 hour ago</td>
-            <td className='pl-5'>
-              <button className='bg-green-500 text-white font-semibold p-1 rounded'>Mark as Read</button>
-            </td>
+              <tr className='h-[50px] '>
+                <th className='text-left pl-4'>Buyer</th>
+                <th className='text-left pl-4'>Last Message</th>
+                <th className='text-left pl-4'>Date</th>
+                <th className='text-left pl-4'>Action</th>
+              </tr>
 
-          </tr>
-        </table>
-      </div>
+              {data?.map(c => (
+                <tr className= {`${((currentUser.isSeller && !c.readBySeller) || (!currentUser.isSeller && !c.readByBuyer)) ? 'bg-green-100 h-[100px]':'bg-white h-[100px]'}`} key={c.id}>
+                  <td className='pl-6'>{currentUser.isSeller ? c.buyerId : c.sellerId}</td>
+                  <td className='pl-4'><Link to={`/message/${c.id}`}>{c?.lastMessage?.substring(0, 100)}...</Link></td>
+                  <td className='pl-6'>{moment(c.updatedAt).fromNow()}</td>
+                  <td className='pl-5'>
+                    {((currentUser.isSeller && !c.readBySeller) || (!currentUser.isSeller && !c.readByBuyer ))&& (
+                    <button onClick={() => hanleRead(c.id)} className='bg-green-500 text-white font-semibold p-2 rounded'>Mark as Read</button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+              </tbody>
+              
+            </table>
+          </div>)}
     </div>
   )
 }
